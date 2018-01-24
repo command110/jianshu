2c51a922dd7e3363eaeed28a3c98d43b6f0d017b
command110 <1106861637@qq.com>
2018-01-24 16:36:00 +0800

    登录注册页面

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,21 +7,21 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'jianshu project' }
+      { hid: 'description', name: 'description', content: 'jianshu' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type:'image/x-icon', href: '/favicon.ico' }
     ]
   },
   /*
   ** Global CSS
   */
-  css: ['~/assets/css/main.css'],
+  css: ['~/assets/css/main.css','~/assets/css/normalize.css','~/assets/css/font-awesome/css/font-awesome.css'],
   /*
   ** Add axios globally
   */
   build: {
-    vendor: ['axios'],
+    vendor: ['axios']
     /*
     ** Run ESLINT on save
     */
---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <b-container>
+            <b-row>
+                <b-col cols="9">9</b-col>
+                <b-col cols="3">3</b-col>
+            </b-row>
+        </b-container>
+    </div>
+</template>
+<script>
+    export default {
+
+    }
+</script>
